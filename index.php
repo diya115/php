e4b29cf1bc419d83cc1a78b5ca06b8481dd1e418
diaa <diaa.rama7i@gmail.com>
2016-12-04 12:31:07 +0400

    ip-v1

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php 
 
-echo "Welcome with first Test from GITHUB";
+$ipAddress=$_SERVER['REMOTE_ADDR'];
+$macAddr=false;
 
+#run the external command, break output into lines
+$arp=`arp -a $ipAddress`;
+$lines=explode("\n", $arp);
 
+#look for the output line describing our IP address
+foreach($lines as $line)
+{
+   $cols=preg_split('/\s+/', trim($line));
+   if ($cols[0]==$ipAddress)
+   {
+       $macAddr=$cols[1];
+   }
+}
 
 ?>
